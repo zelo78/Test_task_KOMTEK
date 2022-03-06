09ff7c725a16a0eb701e2f1925cb663bba2eef9e
Oleg <zelo78@yandex.ru>
2022-03-06 15:43:11 +0300

    Refactor models

--- main/admin.py
+++ main/admin.py
@@ -1,19 +1,19 @@
 from django.contrib import admin
 
-from main.models import Record, Resource, ResourceVersion
+from main.models import Record, Resource, ResourceIdentifier
 
 
 class VersionInline(admin.StackedInline):
-    model = ResourceVersion
+    model = Resource
     extra = 0
 
 
-class ResourceAdmin(admin.ModelAdmin):
-    list_display = ['identifier', 'short_name']
+class ResourceIdentifierAdmin(admin.ModelAdmin):
+    list_display = ['value']
     inlines = [VersionInline]
 
 
-admin.site.register(Resource, ResourceAdmin)
+admin.site.register(ResourceIdentifier, ResourceIdentifierAdmin)
 
 
 class RecordInline(admin.TabularInline):
@@ -21,9 +21,16 @@ class RecordInline(admin.TabularInline):
     extra = 1
 
 
-class ResourceVersionAdmin(admin.ModelAdmin):
+class ResourceAdmin(admin.ModelAdmin):
+    list_display = ['short_name', 'version', 'valid_from', 'identifier']
     inlines = [RecordInline]
 
 
-admin.site.register(ResourceVersion, ResourceVersionAdmin)
-admin.site.register(Record)
+admin.site.register(Resource, ResourceAdmin)
+
+
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ['code', 'value', 'resource']
+
+
+admin.site.register(Record, RecordAdmin)
--- main/models.py
+++ main/models.py
@@ -1,69 +1,70 @@
+from datetime import date
+
 from django.db import models
-from django.utils import timezone
+
+
+class ResourceIdentifier(models.Model):
+    value = models.CharField(
+        max_length=64, unique=True,
+        verbose_name="идентификатор справочника"
+    )
+
+    class Meta:
+        verbose_name = "идентификатор справочника"
+        verbose_name_plural = "идентификаторы справочников"
+        ordering = ['value']
+
+    def __str__(self):
+        return f'Идентификатор {self.value}, ID: {self.id}'
 
 
 class Resource(models.Model):
-    identifier = models.CharField(
-        max_length=64, unique=True,
-        verbose_name="Идентификатор справочника"
-    )
     name = models.CharField(
         max_length=1024,
-        verbose_name="Наименование справочника"
+        verbose_name="наименование справочника"
     )
     short_name = models.CharField(
         max_length=128,
-        verbose_name="Краткое наименование справочника"
+        verbose_name="краткое наименование справочника"
     )
-    description = models.TextField(verbose_name="Описание справочника")
+    description = models.TextField(verbose_name="описание справочника")
+    version = models.CharField(
+        max_length=32,
+        verbose_name="версия справочника"
+    )
+    valid_from = models.DateField(default=date.today, verbose_name="дата начала действия")
+    identifier = models.ForeignKey(
+        ResourceIdentifier, on_delete=models.CASCADE, related_name='versions',
+        verbose_name="идентификатор"
+    )
 
     class Meta:
         verbose_name = "Справочник"
         verbose_name_plural = "Справочники"
-        ordering = ['short_name']
-
-    def __str__(self):
-        return f'{self.short_name}, ID: {self.identifier}'
-
-
-class ResourceVersion(models.Model):
-    resource = models.ForeignKey(
-        Resource, on_delete=models.CASCADE, related_name='versions',
-        verbose_name="Справочник"
-    )
-    version = models.CharField(
-        max_length=32,
-        verbose_name="Версия справочника"
-    )
-    valid_from = models.DateTimeField(default=timezone.now, verbose_name="Дата начала действия")
-
-    class Meta:
-        verbose_name = "Версия справочника"
-        verbose_name_plural = "Версии справочника"
-        ordering = ['-valid_from']
+        ordering = ['identifier', '-valid_from']
         constraints = [
             models.UniqueConstraint(
-                fields=['resource', 'version'],
-                name='unique_version_for_resource'
+                fields=['identifier', 'version'],
+                name='unique_version_for_identifier'
             ),
         ]
 
     def __str__(self):
-        return f'Версия `{self.version}` для `{self.resource.short_name}` от {self.valid_from}'
+        return f'Справочник #{self.identifier.value} `{self.short_name}`, версия `{self.version}` от {self.valid_from}'
 
 
 class Record(models.Model):
-    resource_version = models.ForeignKey(
-        ResourceVersion, on_delete=models.CASCADE, related_name='records',
-        verbose_name="Версия справочника"
-    )
     code = models.CharField(
         max_length=64,
-        verbose_name="Код элемента",
+        verbose_name="код элемента",
     )
     value = models.CharField(
         max_length=256,
-        verbose_name="Значение элемента"
+        verbose_name="значение элемента"
+    )
+    resource = models.ForeignKey(
+        Resource, on_delete=models.CASCADE, related_name='records',
+        verbose_name="справочник"
     )
 
     class Meta:
@@ -72,4 +73,4 @@ class Record(models.Model):
         ordering = ['code']
 
     def __str__(self):
-        return f'{self.code}: {self.value} из справочника {self.resource_version.resource.short_name}'
+        return f'{self.code}: {self.value} из справочника {self.resource.short_name}'
